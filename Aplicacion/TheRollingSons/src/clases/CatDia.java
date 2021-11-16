8c52282d23d581623013fad3c087634f1ea19923
Paris Ramírez Saldaña <paris.140815@gmail.com>
2021-11-16 11:13:29 -0600

    Agregando los paquetes

---
+++ Aplicacion/TheRollingSons/src/clases/CatDia.java
@@ -0,0 +1,18 @@
+package clases;
+public class CatDia {
+    private int idDia;
+    private String dia;
+    
+    public int getIdDia() {
+        return idDia;
+    }
+    public void setIdDia(int idDia) {
+        this.idDia = idDia;
+    }
+    public String getDia() {
+        return dia;
+    }
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+}
