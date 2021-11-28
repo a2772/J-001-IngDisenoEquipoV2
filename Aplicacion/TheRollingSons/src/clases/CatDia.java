241c162c3daf8d1a5f89fc46edea95da7c9a8cb6
Paris Ramírez Saldaña <paris.140815@gmail.com>
2021-11-27 22:05:35 -0600

    Guardando cambios

---
+++ Aplicacion/TheRollingSons/src/clases/CatDia.java
@@ -0,0 +1,34 @@
+package clases;
+public class CatDia {
+    private int idDia;
+    private String dia;
+
+    /**
+     * @return the idDia
+     */
+    public int getIdDia() {
+        return idDia;
+    }
+
+    /**
+     * @param idDia the idDia to set
+     */
+    public void setIdDia(int idDia) {
+        this.idDia = idDia;
+    }
+
+    /**
+     * @return the dia
+     */
+    public String getDia() {
+        return dia;
+    }
+
+    /**
+     * @param dia the dia to set
+     */
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+    
+}
